9a2ed1d1be04bec21fabf1bdc2c7adc96fd0eabb
JesperKJ <jesper.alt@gmail.com>
2018-10-03 23:25:07 +0200

    its working!

--- SequenceAlignment/SequenceAlignment.java
+++ SequenceAlignment/SequenceAlignment.java
@@ -4,12 +4,6 @@ import java.math.*;
 import java.util.regex.Pattern;
 public class SequenceAlignment {
 	
-	//public static ArrayList<double> Alignment(ArrayList<String> X, ArrayList<String> Y){
-		//Array[x.];
-
-		//return shortPoint; //shortPoint;
-	//}
-	
 	static class penalties {
 		public String name;
 		public int[][] matrix;
@@ -99,21 +93,20 @@ public class SequenceAlignment {
 			char[] Yout = new char[Math.max(lengthx, lengthy)];
 			
 			int[][] A = new int[lengthx + 1][lengthy + 1];
-			int[][] Aux = new int[lengthx][lengthy];
+			int[][] Aux = new int[lengthx + 1][lengthy + 1];
 			int d = -4;
 			int opt;
 			int alpha = 0;
 			
 			for (int i = 0; i <= lengthx; i++) {
 				A[i][0] = i*d;
+				Aux[i][0] = 2;
 			}
 			for (int i = 0; i <= lengthy; i++) {
 				A[0][i] = i*d;
+				Aux[0][i] = 3;
 			}
-			System.out.println(X);
-			System.out.println(Y);
-			System.out.println(lengthx);
-			System.out.println(lengthy);
+			Aux[0][0] = 1;
 			for (int i = 1; i <= lengthx; i++) {
 				for (int j = 1; j <= lengthy; j++){
 			alpha = matrix[name.indexOf(X.charAt(i-1))][name.indexOf(Y.charAt(j-1))];
@@ -123,14 +116,14 @@ public class SequenceAlignment {
 			A[i][j] = Math.max(d1, Math.max(d2, d3));
 			
 			if (d1 >= d2 & d1 >= d3)
-			Aux[i-1][j-1] = 1;
+			Aux[i][j] = 1;
 			else if (d2 >= d3)
-				Aux[i-1][j-1] = 2;
+				Aux[i][j] = 2;
 			else
-				Aux[i-1][j-1] = 3;
+				Aux[i][j] = 3;
 				}
 			}
-		
+			/*
 			for (int i = 0 ; i < lengthx +1 ; i++) {
 				for (int j = 0; j < lengthy +1; j++)
 				System.out.print(A[i][j] + " ");
@@ -138,40 +131,38 @@ public class SequenceAlignment {
 			}
 			
 			
-			for (int i = 0 ; i < lengthx ; i++) {
-				for (int j = 0; j < lengthy; j++)
+			for (int i = 0 ; i <= lengthx ; i++) {
+				for (int j = 0; j <= lengthy; j++)
 				System.out.print(Aux[i][j] + " ");
 				System.out.println();
 			}
-			
-			int lx = lengthx - 1;
-			int ly = lengthy - 1;
+			*/
+			int lx = lengthx;
+			int ly = lengthy;
 			for (int i = Math.max(lengthx, lengthy)-1; i>=0;i--) { 
 			if (Aux[lx][ly] == 1) {
-			Xout[i] = X.charAt(lx);
-			Yout[i] = Y.charAt(ly);
+			Xout[i] = X.charAt(lx-1);
+			Yout[i] = Y.charAt(ly-1);
 			if (lx > 0) 
 				lx--;
 			if (ly > 0)
 				ly--;
 			}
 			else if (Aux[lx][ly] == 2) {
-			Xout[i] = X.charAt(lx);
+			Xout[i] = X.charAt(lx-1);
 			Yout[i] = '-';
 			if (lx > 0) 
 				lx--;
 			}
 			else if (Aux[lx][ly] == 3) {
 				Xout[i] = '-';
-				Yout[i] = Y.charAt(ly);
+				Yout[i] = Y.charAt(ly-1);
 			if (ly > 0)
 				ly--;
 			}
 			}
 			String XoutS = String.valueOf(Xout);
 			String YoutS = String.valueOf(Yout);
-			//System.out.println(Xout);
-			//System.out.println(Yout);
 			matchSpecies.add(new species("a",XoutS,A[lengthx][lengthy]));
 			matchSpecies.add(new species("a",YoutS,A[lengthx][lengthy]));
 			return matchSpecies;
@@ -195,61 +186,49 @@ public class SequenceAlignment {
 	
 	
 	public static void main(String[] args) throws IOException {
-	    penalties inputData = dataReader(args[0]);// System.in);
-	  //  input inputData2 = readFile(args[0]);// System.in);
+	    penalties inputData = dataReader(args[0]);
 		
 	    ArrayList<String> names = new ArrayList<>();
 		ArrayList<String> gene = new ArrayList<>();
+		int nfiles = args.length - 1;
+		while (nfiles >= 0) {
+			String protein = "";
 		try {
 			Scanner scan = null;
-			File file = new File(args[0]);
+			File file = new File(args[nfiles]);
 			scan = new Scanner(file);
 			while (scan.hasNextLine()) {
 				final String line = scan.nextLine().trim();
-				if (line.contains(">"))
-					names.add(line);
+				if (line.contains(">")) {
+					if (protein.length()>0) { 
+						gene.add(protein);
+						protein = "";
+					}
+					names.add(line.replaceAll(">","").split("\\s+")[0]);
+				}
 				else
-					gene.add(line);
+					protein = protein + line;
 			}
+			gene.add(protein);
+
 		} catch(FileNotFoundException e) {
 			e.printStackTrace();
 		}
-	    /*
-		for (int i = 0 ; i < names.size() ; i++) 
-			System.out.print(names.get(i) + " ");
-			System.out.println();	
-			
-			for (int i = 0 ; i < gene.size() ; i++) 
-				System.out.print(gene.get(i) + " ");
-				System.out.println();	
-		*/
-		/*
-		for (int i = 0 ; i < inputData.names.length ; i++) 
-			System.out.print(inputData.names[i] + " ");
-			System.out.println();	
-			
-		for (int i = 0 ; i < inputData.matrix.length ; i++) {
-			for (int j = 0; j < inputData.matrix.length; j++)
-			System.out.print(inputData.matrix[i][j] + " ");
-			System.out.println();
-		}
-		*/
-				
+		nfiles--;
+		}				
 		//Algorithm: Two strings as input, double as output
 				
 				int I = 2;
 				int J = 2;
 				ArrayList<species> matchSpecies = new ArrayList<>();
-				//System.out.println(opt(gene.get(0), gene.get(1), inputData.name, inputData.matrix, I, J));
 			for (int i = 0; i < gene.size(); i++){
-				for (int j = i + 1; j < gene.size(); j++) {
+				for (int j = i + 1; j +1 < gene.size(); j++) {
 					matchSpecies = AbuttomUp(gene.get(i),gene.get(j), inputData.name, inputData.matrix);
 				System.out.println(names.get(i) + "--" + names.get(j) + ": " + matchSpecies.get(1).penalty);
 				System.out.println(matchSpecies.get(0).protein);
 				System.out.println(matchSpecies.get(1).protein);
 				}
 			}
-				//System.out.println(inputData.name);
 	}	
 	
 	}
